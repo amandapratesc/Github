5304b453978ba62b48863df680a58833f7374e06
nataliakrein <natikrein@gmail.com>
2023-09-26 08:57:41 -0300

    teste

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 console.log('teste');
-<<<<<<< HEAD
 console.log('conflito???????');
-=======
 console.log('testando um teste de teste');
 console.log('teste2');
->>>>>>> 2de75dfa158f62276b29170f1f7ffba500dc6b10
